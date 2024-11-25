6903f50d8c714b49658aca934f8c4c799f10a172
Ming0512 <150341190+Ming0512@users.noreply.github.com>
2024-11-25 08:54:30 +0530

    Update headshots_nrg.py

--- headshots_nrg.py
+++ headshots_nrg.py
@@ -2,7 +2,6 @@ import cv2
 
 name = 'MINGMA_21EC39043' 
 
-# Initialize the camera
 cam = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -17,32 +16,23 @@ while True:
     if not ret:
         print("Failed to grab frame")
         break
-
-    # Convert frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces
+    
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Draw rectangles around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
 
-    # Show the frame with rectangles
     cv2.imshow("press space to take a photo", frame)
 
     k = cv2.waitKey(1)
     if k % 256 == 27:
-        # ESC pressed
         print("Escape hit, closing...")
         break
     elif k % 256 == 32:
-        # SPACE pressed
         img_name = "dataset/" + name + "/image_{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
-
-# Release the camera and close the windows
 cam.release()
 cv2.destroyAllWindows()
